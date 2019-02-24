c8b90d81d55b55b881f83ecd4d7ffe245b354a70
John Chang <johnchang9094@gmail.com>
2019-02-24 08:47:54 +0000

    Refactor to lambda with Consumer

--- src/main/java/MainQuote.java
+++ src/main/java/MainQuote.java
@@ -1,3 +1,5 @@
+import java.util.function.Consumer;
+
 import exception.UnavailableLoanException;
 import model.ClosingQuote;
 import org.slf4j.Logger;
@@ -8,22 +10,37 @@ import service.CalculationService;
 import static utils.Utils.INVALID_AMOUNT;
 import static utils.Utils.isValidRequestedAmount;
 
-public class MainQuote {
+public class MainQuote implements Runnable {
     private static final Logger log = LoggerFactory.getLogger(MainQuote.class);
 
-    public static void main(String[] args) {
+    private String csvFile;
+    private Integer requestedAmount;
+    private Consumer<ClosingQuote> printQuote;
 
-        try {
-            String data = args[0];
-            Integer requestedAmount = Integer.valueOf(args[1]);
-            if (isValidRequestedAmount(requestedAmount)) {
-                ClosingQuote<Calculation> closingQuote = new ClosingQuote<>(requestedAmount, data, new CalculationService());
-                System.out.println(closingQuote.toString());
-            } else {
+    private MainQuote(String csvFile, Integer requestedAmount) {
+        this.csvFile = csvFile;
+        this.requestedAmount = requestedAmount;
+        printQuote = this::printQuoteWithDetails;
+    }
+
+    public static void main(String[] args) {
+        new MainQuote(args[0], Integer.valueOf(args[1])).run();
+    }
+
+    public void run() {
+        if (isValidRequestedAmount(requestedAmount)) {
+            ClosingQuote<Calculation> closingQuote = new ClosingQuote<>(requestedAmount, csvFile, new CalculationService());
+            printQuote.accept(closingQuote);
+        } else {
+            try {
                 throw new UnavailableLoanException(INVALID_AMOUNT);
+            } catch (UnavailableLoanException e) {
+                log.error("Error: ", e);
             }
-        } catch (UnavailableLoanException e) {
-            log.error("Error: ", e);
         }
+    }
+
+    private void printQuoteWithDetails(ClosingQuote quote) {
+        System.out.println(quote);
     }
 }
